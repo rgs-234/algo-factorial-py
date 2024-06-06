bc2c395ffda4e12f4c679c654322b7f875158cb6
rgs-234 <rachelgsnyder@gmail.com>
2024-06-06 18:26:43 -0500

    recursive/non-recursive solutions to factorial assignment

--- factorial.py
+++ factorial.py
@@ -1,5 +1,17 @@
+
+# # Recursive solution
+# def factorial(num):
+# 	if num == 2:
+# 		return num
+# 	else:
+# 		return num * factorial(num - 1)
+	
+
+# Non-recursive solution
 def factorial(num):
-	if num == 2:
-		return num
-	else:
-		return num * factorial(num - 1)
+    result = 1
+    
+    for num in range(1, num + 1):
+        result *= num
+        
+    return result
